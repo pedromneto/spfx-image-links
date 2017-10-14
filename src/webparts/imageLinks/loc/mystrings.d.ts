b5795ea4914b8061190ab33825587488f9b770bf
Pedro Marques M. Neto <pedro.neto@outlook.com>
2017-10-14 17:44:08 -0300

    Criação do projeto

---
+++ src/webparts/imageLinks/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IImageLinksWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ImageLinksWebPartStrings' {
+  const strings: IImageLinksWebPartStrings;
+  export = strings;
+}
---
+++ src/webparts/imageLinks/test/ImageLinksWebPart.test.ts
@@ -0,0 +1,9 @@
+/// <reference types="mocha" />
+
+import { assert } from 'chai';
+
+describe('ImageLinksWebPart', () => {
+  it('should do something', () => {
+    assert.ok(true);
+  });
+});
